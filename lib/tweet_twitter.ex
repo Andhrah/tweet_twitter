1b3fb77bbd027424577d1c5c95752a152ea48e2a
Alexandra Collins <andracolexa@gmail.com>
2021-12-22 18:06:18 +0100

    project file

---
+++ lib/tweet_twitter.ex
@@ -0,0 +1,18 @@
+defmodule TweetTwitter do
+  @moduledoc """
+  Documentation for `TweetTwitter`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TweetTwitter.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
